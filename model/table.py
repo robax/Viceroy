508598c939db2854e3c80f4ec2b33dfca8c1f490
robax <robertwalters@email.arizona.edu>
2016-06-14 11:54:44 -0700

    Started parsing hand xml

--- model/table.py
+++ model/table.py
@@ -1,11 +1,47 @@
+from xml.dom import minidom
+
 class Table:
 	pot = 0
 	players = 0
+	cardOne = ""
+	cardTwo = ""
+	handClass = ""
+	dealer = 0
 	
 	def __init__(self):
-                self.data = []
+		self.pot = 0
 
-	def reset():
-		this.pot = 0
-		this.players = 0
-        print "hello"
+	def reset(self):
+		print "reset"
+
+	def getHandClass():
+		if(cardOne[1]==cardTwo[1]):
+			handClass = cardOne[0] , cardTwo[0] , "s"
+		else:
+			handClass = cardOne[0] , cardTwo[0] , "o"
+
+	def out(self):
+		print "pot: " , self.pot
+		print "players: " , self.players
+		print "hand: " , self.cardOne , " " , self.cardTwo
+
+	def getPlayers():
+		num = 0
+		players = dom.getElementsByTagName('table')
+		for char in players.getAttribute('states'):
+			if char=='p':
+				num += 1
+		return num
+
+	def parse(self, data):
+		print data
+		dom = minidom.parse("hands.txt")
+		if "newhand" in data:
+			self.reset()
+			# get # players
+			# get new hand
+			if "pcard" in data:
+				print "test"
+				cards = dom.getElementsByTagName('pcard')
+				self.cardOne = cards[0][0:1]
+				self.cardTwo = cards[1][0:1]
--- viceroy.py
+++ viceroy.py
@@ -1,23 +1,109 @@
-from xml.dom import minidom
-from networking import net
-from model import table
-
-shit = """<endhand />
+data="""<root><playeraction action="call" amount="1400" seat="1" />
+<pot pot="1600" seat="1" total_pot="4155" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="4500" gold="" nick="lindaweaver" seat="1" state="p" /><endask /><tcard card="8d" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: Dealt to board [ 8d ]" type="dealer" /><betstopot pot="4100" rake="100" side="" />
+<timeout seat="1" seconds="19" />
+<playeraction action="check" seat="1" />
+<timeout seat="6" seconds="19" />
+<ack />
+<playeraction action="bet" amount="4200" seat="6" />
+<pot pot="4200" seat="6" total_pot="8300" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="24808" gold="" nick="Drummer1946" seat="6" state="p" /><timeout seat="1" seconds="19" />
+<playeraction action="call" amount="4200" seat="1" />
+<pot pot="4200" seat="1" total_pot="12500" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="300" gold="" nick="lindaweaver" seat="1" state="p" /><endask /><betstopot pot="12500" rake="100" side="" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: Drummer1946 shows [ 2s 4s ] Full House, Twos full of Queens" type="dealer" /><showcards cards="2s,4s" seat="6" /><playeraction action="show" seat="6" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: lindaweaver shows [ 2d Ad ] Full House, Twos full of Queens" type="dealer" /><showcards cards="2d,ad" seat="1" /><playeraction action="show" seat="1" />
+<chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: &lt;b&gt;lindaweaver wins 6,250 chips with Full House, Twos full of Queens&lt;/b&gt;" type="dealer" />
+<chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: &lt;b&gt;Drummer1946 wins 6,250 chips with Full House, Twos full of Queens&lt;/b&gt;" type="dealer" /><chipsonhand chips="0" winner="lindaweaver" />
+<autopre end="1" /><potstoplayers pots="0,6250,0,0,0,0,6250,0,0" rake="100" /><endhand />
 <table available=",,,,,,,," bb="50" bots=",,,,,,,," chips="6000,6550,13960,3147,6100,4639,31058,19230,6250" dealer="6" gold=",,,,,,,," max_bring="10000" nicks="i9iveup,lindaweaver,robax,Nino23killer,anihimi,tarnett,Drummer1946,26dealmein,Lorraine88" sb="25" spectators="2" states="p,p,p,p,p,p,p,p,p" user_ids="1002369,347098,1075655,1134695,773244,499282,368900,1050154,788799" />
+
 <newhand hand="214009455" prev="214009253" /><pot pot="50" seat="5" total_pot="50" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/111296/medium/Picture_1178.jpg?1420709860" bot="" chips="4589" gold="" nick="tarnett" seat="5" state="p" /><playeraction action="post bb" amount="50" seat="5" /><noask /><table available=",,,,,,,," bb="50" bots=",,,,,,,," chips="6000,6550,13960,3147,6100,4589,31058,19230,6250" dealer="7" gold=",,,,,,,," max_bring="10000" nicks="i9iveup,lindaweaver,robax,Nino23killer,anihimi,tarnett,Drummer1946,26dealmein,Lorraine88" sb="25" spectators="2" states="p,p,p,p,p,p,p,p,p" user_ids="1002369,347098,1075655,1134695,773244,499282,368900,1050154,788799" />
 <pretext pd1="Auto Post Blinds" pd2="Auto Muck Hands" pd3="Sit Out Next Hand" pd4="Fold To Any Bet" /><pre pre="pd1" show="true" state="1" /><pre pre="pd2" show="true" state="1" /><pre pre="pd3" show="true" state="0" /><pre pre="pd4" show="true" state="0" /><dealer seat="7" /><pot pot="25" seat="0" total_pot="75" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/222659/medium/296551_104926816277480_7462233_n.jpg?1455182001" bot="" chips="5975" gold="" nick="i9iveup" seat="0" state="p" /><playeraction action="post sb" amount="25" seat="0" /><pot pot="50" seat="1" total_pot="125" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="6500" gold="" nick="lindaweaver" seat="1" state="p" /><playeraction action="post bb" amount="50" seat="1" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: ** Hand [ 214009455 ] started **" type="dealer" /><pcard seat="0" /><pcard seat="1" />
 <pcard card="jd" seat="2" /><pcard seat="3" /><pcard seat="4" /><pcard seat="5" /><pcard seat="6" /><pcard seat="7" /><pcard seat="0" /><pcard seat="1" /><pcard card="8c" seat="2" /><pcard seat="3" /><pcard seat="4" /><pcard seat="5" /><pcard seat="6" /><pcard seat="7" /><autopre end="1" /><endask /><endask /><ask allin="0" bb="50" display=",50,100" hand_id="214009455" maxraise="13960" quick_bet_labels="x2,x3,POT,ALL IN" quick_bet_values="100,150,225,13960" reply="fold,call,100" slide="1" text="FOLD,CALL,RAISE TO" /><timeout seat="2" seconds="19" />
-"""
+
+
+<ack />
+
+<playeraction action="call" amount="50" seat="2" />
+<pot pot="50" seat="2" total_pot="175" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/249187/medium/YYhEqa7DR9C66M7GDk7d.jpg?1461354718" bot="" chips="13910" gold="" nick="robax" seat="2" state="p" /><endask /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Raise to 100,,Call any,Raise any" /><timeout seat="3" seconds="19" />
+<playeraction action="call" amount="50" seat="3" />
+<pot pot="50" seat="3" total_pot="225" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="3097" gold="" nick="Nino23killer" seat="3" state="p" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Raise to 100,,Call any,Raise any" /><playeraction action="call" amount="50" seat="4" /><pot pot="50" seat="4" total_pot="275" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/163693/medium/Photo_00003.jpg?1440179578" bot="" chips="6050" gold="" nick="anihimi" seat="4" state="p" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Raise to 100,,Call any,Raise any" /><playeraction action="check" seat="5" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Raise to 100,,Call any,Raise any" /><timeout seat="6" seconds="19" />
+<playeraction action="call" amount="50" seat="6" />
+<pot pot="50" seat="6" total_pot="325" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="31008" gold="" nick="Drummer1946" seat="6" state="p" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Raise to 100,,Call any,Raise any" /><playeraction action="call" amount="50" seat="7" /><pot pot="50" seat="7" total_pot="375" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="19180" gold="" nick="26dealmein" seat="7" state="p" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Raise to 100,,Call any,Raise any" /><timeout seat="0" seconds="19" />
+<ack />
+<playeraction action="raise" amount="225" seat="0" />
+<pot pot="250" seat="0" total_pot="600" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/222659/medium/296551_104926816277480_7462233_n.jpg?1455182001" bot="" chips="5750" gold="" nick="i9iveup" seat="0" state="p" /><autopre data=",200,400,fold,call,raise" display=",Call (200),Raise to 450,Fold,Call any,Raise any" /><timeout seat="1" seconds="19" />
+<playeraction action="call" amount="200" seat="1" />
+<pot pot="250" seat="1" total_pot="800" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="6300" gold="" nick="lindaweaver" seat="1" state="p" /><endask /><endask /><ask allin="0" bb="50" display=",200,450" hand_id="214009455" maxraise="13960" quick_bet_labels="MIN,1/2 POT,POT,ALL IN" quick_bet_values="400,750,1250,13960" reply="fold,call,450" slide="1" text="FOLD,CALL,RAISE TO" /><timeout seat="2" seconds="19" />
+
+<playeraction action="call" amount="200" seat="2" />
+<pot pot="250" seat="2" total_pot="1000" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/249187/medium/YYhEqa7DR9C66M7GDk7d.jpg?1461354718" bot="" chips="13710" gold="" nick="robax" seat="2" state="p" /><endask /><autopre data="checkfold,check,200,,call,raise" display="Check/fold,Check,Raise to 450,,Call any,Raise any" /><timeout seat="3" seconds="19" />
+<ack />
+<playeraction action="call" amount="200" seat="3" />
+<pot pot="250" seat="3" total_pot="1200" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="2897" gold="" nick="Nino23killer" seat="3" state="p" /><autopre data="checkfold,check,200,,call,raise" display="Check/fold,Check,Raise to 450,,Call any,Raise any" /><playeraction action="call" amount="200" seat="4" /><pot pot="250" seat="4" total_pot="1400" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/163693/medium/Photo_00003.jpg?1440179578" bot="" chips="5850" gold="" nick="anihimi" seat="4" state="p" /><autopre data="checkfold,check,200,,call,raise" display="Check/fold,Check,Raise to 450,,Call any,Raise any" /><timeout seat="5" seconds="19" />
+<fold seat="5" />
+<playeraction action="fold" seat="5" /><autopre data="checkfold,check,200,,call,raise" display="Check/fold,Check,Raise to 450,,Call any,Raise any" /><timeout seat="6" seconds="19" />
+<playeraction action="call" amount="200" seat="6" />
+<pot pot="250" seat="6" total_pot="1600" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="30808" gold="" nick="Drummer1946" seat="6" state="p" /><autopre data="checkfold,check,200,,call,raise" display="Check/fold,Check,Raise to 450,,Call any,Raise any" /><fold seat="7" /><playeraction action="fold" seat="7" /><autopre data="checkfold,check,200,,call,raise" display="Check/fold,Check,Raise to 450,,Call any,Raise any" /><endask /><autopre end="1" /><tcard card="Ah" /><tcard card="Jc" /><tcard card="7d" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: Dealt to board [ Ah Jc 7d ]" type="dealer" /><handtext board_cards="Jc,Ah,7d" handtext="You have: Pair, Jacks" player_cards="Jd,8c" /><betstopot pot="1520" rake="80" side="" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="0" seconds="19" />
+<ack />
+<playeraction action="check" seat="0" />
+<autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="1" seconds="19" />
+<playeraction action="check" seat="1" />
+<endask /><endask /><ask allin="0" bb="50" display=",,50" hand_id="214009455" maxraise="13710" quick_bet_labels="MIN,1/2 POT,POT,ALL IN" quick_bet_values="50,800,1600,13710" reply="fold,check,50" slide="1" text="FOLD,CHECK,BET" /><timeout seat="2" seconds="19" />
+
+<playeraction action="check" seat="2" />
+<endask /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="3" seconds="19" />
+<playeraction action="check" seat="3" />
+<autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><playeraction action="check" seat="4" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="6" seconds="19" />
+<ack />
+<playeraction action="check" seat="6" />
+<endask /><autopre end="1" /><tcard card="9h" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: Dealt to board [ 9h ]" type="dealer" /><handtext board_cards="Jc,Ah,9h" handtext="You have: Pair, Jacks" player_cards="Jd,8c" /><betstopot pot="1520" rake="80" side="" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="0" seconds="19" />
+<ack />
+<playeraction action="check" seat="0" />
+<autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="1" seconds="19" />
+<playeraction action="bet" amount="251" seat="1" />
+<pot pot="251" seat="1" total_pot="1771" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="6049" gold="" nick="lindaweaver" seat="1" state="p" /><endask /><endask /><ask allin="0" bb="50" display=",251,502" hand_id="214009455" maxraise="13710" quick_bet_labels="MIN,1/2 POT,POT,ALL IN" quick_bet_values="502,1302,2353,13710" reply="fold,call,502" slide="1" text="FOLD,CALL,RAISE TO" /><timeout seat="2" seconds="19" />
+<ack />
+
+<playeraction action="call" amount="251" seat="2" />
+<pot pot="251" seat="2" total_pot="2022" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/249187/medium/YYhEqa7DR9C66M7GDk7d.jpg?1461354718" bot="" chips="13459" gold="" nick="robax" seat="2" state="p" /><endask /><autopre data="checkfold,check,251,,call,raise" display="Check/fold,Check,Raise to 502,,Call any,Raise any" /><timeout seat="3" seconds="19" />
+<playeraction action="call" amount="251" seat="3" />
+<pot pot="251" seat="3" total_pot="2273" /><seat available="1" avatar_url="avatars/male.png" bot="" chips="2646" gold="" nick="Nino23killer" seat="3" state="p" /><autopre data="checkfold,check,251,,call,raise" display="Check/fold,Check,Raise to 502,,Call any,Raise any" /><timeout seat="4" seconds="19" />
+<ack />
+<fold seat="4" />
+<playeraction action="fold" seat="4" /><autopre data="checkfold,check,251,,call,raise" display="Check/fold,Check,Raise to 502,,Call any,Raise any" /><fold seat="6" /><playeraction action="fold" seat="6" /><autopre data="checkfold,check,251,,call,raise" display="Check/fold,Check,Raise to 502,,Call any,Raise any" /><timeout seat="0" seconds="19" />
+<ack />
+<playeraction action="call" amount="251" seat="0" />
+<pot pot="251" seat="0" total_pot="2524" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/222659/medium/296551_104926816277480_7462233_n.jpg?1455182001" bot="" chips="5499" gold="" nick="i9iveup" seat="0" state="p" /><autopre data="checkfold,check,251,,call,raise" display="Check/fold,Check,Raise to 502,,Call any,Raise any" /><endask /><autopre end="1" /><tcard card="7c" /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: Dealt to board [ 7c ]" type="dealer" /><handtext board_cards="Jc,7d,7c,Ah" handtext="You have: Two Pair, Jacks and Sevens" player_cards="Jd" /><betstopot pot="2504" rake="100" side="" /><autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="0" seconds="19" />
+<playeraction action="check" seat="0" />
+<autopre data="checkfold,check,50,,call,raise" display="Check/fold,Check,Bet (50),,Call any,Raise any" /><timeout seat="1" seconds="19" />
+<ack />
+<playeraction action="check" seat="1" />
+<endask /><endask /><ask allin="0" bb="50" display=",,50" hand_id="214009455" maxraise="13459" quick_bet_labels="MIN,1/2 POT,POT,ALL IN" quick_bet_values="50,1302,2604,13459" reply="fold,check,50" slide="1" text="FOLD,CHECK,BET" /><timeout seat="2" seconds="19" />
+
+<playeraction action="bet" amount="1302" seat="2" />
+<pot pot="1302" seat="2" total_pot="3806" /><seat available="1" avatar_url="https://replaypoker.s3.amazonaws.com/assets/avatars/249187/medium/YYhEqa7DR9C66M7GDk7d.jpg?1461354718" bot="" chips="12157" gold="" nick="robax" seat="2" state="p" /><endask /><autopre data="checkfold,check,1302,,call,raise" display="Check/fold,Check,Raise to 2604,,Call any,Raise any" /><timeout seat="3" seconds="19" />
+<fold seat="3" />
+<playeraction action="fold" seat="3" /><autopre data="checkfold,check,1302,,call,raise" display="Check/fold,Check,Raise to 2604,,Call any,Raise any" /><fold seat="0" /><playeraction action="fold" seat="0" /><autopre data="checkfold,check,1302,,call,raise" display="Check/fold,Check,Raise to 2604,,Call any,Raise any" /><fold seat="1" /><playeraction action="fold" seat="1" /><endask /><chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: robax mucks" type="dealer" /><playeraction action="muck" seat="2" />
+<ack />
+<chat from="" raw_text="" text="&lt;b&gt;Dealer&lt;/b&gt;: &lt;b&gt;robax wins 2,504 chips&lt;/b&gt;" type="dealer" />
+<chipsonhand chips="2003" winner="robax" />
+<autopre end="1" /><potstoplayers pots="0,0,3806,0,0,0,0,0,0" rake="100" /><endhand />
+</root>"""
+
+from networking import net
+from model import table
 
 if __name__ == "__main__":
-    print "Searching for port..."
+    #print "Searching for port..."
     #port = net.findPort()
     #print "Port found! " , port
     #print "Searching for ip..."
     #ip = net.findIP()
     #print "ip found! " , ip
-    table = table.Table()
-    xmldoc = minidom.parse(shit)
+    state = table.Table()
+
+    state.parse(data)
+
+    state.out()
+    
+
     #while True:
     #    print net.getPacket(port, ip)
 
