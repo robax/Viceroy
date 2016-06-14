f3833e2f1ef4eaa748ea5ad197ca0bc7f5f33975
robax <robertwalters@email.arizona.edu>
2016-06-14 13:52:24 -0700

    More progress on xml parsing

--- model/table.py
+++ model/table.py
@@ -1,47 +1,56 @@
 from xml.dom import minidom
 
 class Table:
-	pot = 0
-	players = 0
-	cardOne = ""
-	cardTwo = ""
-	handClass = ""
-	dealer = 0
 	
 	def __init__(self):
 		self.pot = 0
+		self.players = 0
+		self.hand = ["",""]
+		self.handClass = ""
+		self.dealer = 0
 
 	def reset(self):
 		print "reset"
 
-	def getHandClass():
-		if(cardOne[1]==cardTwo[1]):
-			handClass = cardOne[0] , cardTwo[0] , "s"
+	def setHandClass(self):
+		if(self.hand[0][1]==self.hand[1][1]):
+			self.handClass = self.hand[0][0] , self.hand[1][0] , "s"
 		else:
-			handClass = cardOne[0] , cardTwo[0] , "o"
+			self.handClass = str(self.hand[0][0]) + str(self.hand[1][0]) + "o"
 
 	def out(self):
-		print "pot: " , self.pot
-		print "players: " , self.players
-		print "hand: " , self.cardOne , " " , self.cardTwo
+		print "pot:" , self.pot
+		print "players:" , self.players
+		print "hand:" , self.hand[0] , self.hand[1]
+		print "hand class:" , self.handClass
 
-	def getPlayers():
+	def getPlayers(self, players):
 		num = 0
-		players = dom.getElementsByTagName('table')
-		for char in players.getAttribute('states'):
-			if char=='p':
-				num += 1
-		return num
+		for node in players:
+			if("p" in node.getAttribute('states')):
+				for char in node.getAttribute('states'):
+					if char=='p':
+						num += 1
+				return num
+	def setPot(self, pots):
+		for newPot in pots:
+			if(newPot.getAttribute('total_pot') != ""):
+				self.pot = newPot.getAttribute('total_pot')
 
 	def parse(self, data):
-		print data
+		# parse the packet data
 		dom = minidom.parse("hands.txt")
+		# reset state if theres a new hand
 		if "newhand" in data:
 			self.reset()
-			# get # players
-			# get new hand
+			self.players = self.getPlayers(dom.getElementsByTagName('table'))
 			if "pcard" in data:
-				print "test"
 				cards = dom.getElementsByTagName('pcard')
-				self.cardOne = cards[0][0:1]
-				self.cardTwo = cards[1][0:1]
+				cardsSet = 0
+				for node in cards:
+					if(node.getAttribute('card')!=""):
+						self.hand[cardsSet]=node.getAttribute('card')
+						cardsSet += 1
+				self.setHandClass()
+			self.setPot(dom.getElementsByTagName('pot'))
+		
